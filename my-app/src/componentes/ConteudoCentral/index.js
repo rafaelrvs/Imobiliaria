151517540974ace367827d2796eef32a03e9b4cb
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-05 21:06:15 -0300

    adição de layout

--- my-app/src/componentes/ConteudoCentral/index.js
+++ my-app/src/componentes/ConteudoCentral/index.js
@@ -12,25 +12,20 @@ const ConteudoCentral =()=>{
   const imagem6 ='../ImagemDasCasasEpdf/PlantaImovel.png'
   const imagem7 ='../ImagemDasCasasEpdf/tons.png'
 
-
- 
-
-
-
   return(
-    <div>
+    <div className='container-all-img'>
       <div  className='box__header'>
         <div id='Container1'><ImagemDasCasas/></div>
       </div>
         <div className='container__main'>
-
-        <div><img id="imagem" src={imagem1} alt="" /></div>
-        <div><img id="imagem" src={imagem2} alt="" /></div>
-        <div><img id="imagem" src={imagem3} alt="" /></div>
-        <div><img id="imagem" src={imagem4} alt="" /></div>
-        <div><img id="imagem" src={imagem5} alt="" /></div>
-        <div><img id="imagem" src={imagem6} alt="" /></div>
-        <div><img id="imagem" src={imagem7} alt="" /></div>
+  
+        <div id='container__particao__img' ><img id="imagem" src={imagem1} alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem2} alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem3} alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem4} alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem5} alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem6} alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem7} alt="" /></div>
         </div>
 
     </div>
