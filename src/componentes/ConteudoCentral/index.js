c4b952ebb4b03c8f93a5eb20e36776b91e07d641
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-05 23:37:30 -0300

    update

--- src/componentes/ConteudoCentral/index.js
+++ src/componentes/ConteudoCentral/index.js
@@ -4,13 +4,13 @@ import './ConteudoCentral.css';
 
 const ConteudoCentral =()=>{
   
- // const imagem1 ='../ImagemDasCasasEpdf/img/academia.png';
- // const imagem2 ='../ImagemDasCasasEpdf/img/apartamentoFrente.png';
- // const imagem3 ='../ImagemDasCasasEpdf/img/apInside.png';
- // const imagem4 ='../ImagemDasCasasEpdf/img/interna.png';
- // const imagem5 ='../ImagemDasCasasEpdf/img/newFoto.png';
- // const imagem6 ='../ImagemDasCasasEpdf/img/PlantaImovel.png';
- // const imagem7 ='../ImagemDasCasasEpdf/img/tons.png';
+  const imagem1 ='../ImagemDasCasasEpdf/img/academia.png';
+  const imagem2 ='../ImagemDasCasasEpdf/img/apartamentoFrente.png';
+  const imagem3 ='../ImagemDasCasasEpdf/img/apInside.png';
+  const imagem4 ='../ImagemDasCasasEpdf/img/interna.png';
+  const imagem5 ='../ImagemDasCasasEpdf/img/newFoto.png';
+  const imagem6 ='../ImagemDasCasasEpdf/img/PlantaImovel.png';
+  const imagem7 ='../ImagemDasCasasEpdf/img/tons.png';
 
   return(
     <div className='container-all-img'>
@@ -19,13 +19,13 @@ const ConteudoCentral =()=>{
       </div>
         <div className='container__main'>
   
-        <div id='container__particao__img' ><img id="imagem" src='../ImagemDasCasasEpdf/img/academia.png' alt="" /></div>
-        <div id='container__particao__img' ><img id="imagem" src='../ImagemDasCasasEpdf/img/apartamentoFrente.png' alt="" /></div>
-        <div id='container__particao__img' ><img id="imagem" src='../ImagemDasCasasEpdf/img/apInside.png' alt="" /></div>
-        <div id='container__particao__img' ><img id="imagem" src='../ImagemDasCasasEpdf/img/interna.png' alt="" /></div>
-        <div id='container__particao__img' ><img id="imagem" src='../ImagemDasCasasEpdf/img/newFoto.png' alt="" /></div>
-        <div id='container__particao__img' ><img id="imagem" src='../ImagemDasCasasEpdf/img/PlantaImovel.png' alt="" /></div>
-        <div id='container__particao__img' ><img id="imagem" src='../ImagemDasCasasEpdf/img/tons.png' alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem1}/></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem2} alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem3} /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem4} alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem5} alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem6}alt="" /></div>
+        <div id='container__particao__img' ><img id="imagem" src={imagem7} /></div>
         </div>
 
     </div>
